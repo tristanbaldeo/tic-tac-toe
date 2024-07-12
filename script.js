0f736faca0cc162642b70ea1e25a14d379ac58f8
tristanbaldeo <113404111+tristanbaldeo@users.noreply.github.com>
2024-07-12 19:27:14 -0400

    created gameboard object and added rendering within terminal

--- script.js
+++ script.js
@@ -13,8 +13,22 @@ const player2 = new Player('Player 2', 'O');
 currentPlayer = player1;
 
 // Create gameboard as an array inside of object
+const gameBoard = {
+    board: ['','','','','','','','',''],
+    display: function() {
+        console.log(`${this.board[0]}   | ${this.board[1]} |   ${this.board[2]}`)
+        console.log(`----------`)
+        console.log(`${this.board[3]}   | ${this.board[4]} |   ${this.board[5]}`)
+        console.log(`----------`)
+        console.log(`${this.board[6]}   | ${this.board[7]} |   ${this.board[8]}`)
+    },
+    reset: function() {
+        this.board = ['','','','','','','','',''];
+    }
+};
 
 // Render game board in terminal
+gameBoard.display();
 
 // Function that receives player input and switches between players after each move
 
