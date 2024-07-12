4c49740ac5c164bf3a3d59d5c77997471b84bac8
tristanbaldeo <113404111+tristanbaldeo@users.noreply.github.com>
2024-07-12 17:20:59 -0400

    created player objects for game flow

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 // Initialize game state and current player
+let GameOver = false;
+let currentPlayer = null;
+
+// Create and store players as objects that control game flow
+function Player(name, marker) {
+    this.name = name;
+    this.marker = marker;
+}
+
+const player1 = new Player('Player 1', 'X');
+const player2 = new Player('Player 2', 'O');
+currentPlayer = player1;
 
 // Create gameboard as an array inside of object
 
 // Render game board in terminal
-
-// Create and store players as objects that contorl game flow
 
 // Function that receives player input and switches between players after each move
 
