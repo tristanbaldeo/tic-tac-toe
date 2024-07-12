0d65c52175502f53187d45a4a97b6bfe163d786f
tristanbaldeo <113404111+tristanbaldeo@users.noreply.github.com>
2024-07-12 16:17:04 -0400

    established layout steps for functionality of the game within the terminal

---
+++ script.js
@@ -0,0 +1,13 @@
+// Initialize game state and current player
+
+// Create gameboard as an array inside of object
+
+// Render game board in terminal
+
+// Create and store players as objects that contorl game flow
+
+// Function that receives player input and switches between players after each move
+
+// Function that checks for when game is over
+
+// Function that checks for all winning 3-in-a-rows and tied games
