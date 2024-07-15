89bd954fcfd1f02987f0e13a59828b5733c8e810
tristanbaldeo <113404111+tristanbaldeo@users.noreply.github.com>
2024-07-14 22:11:48 -0400

    minor changes made to the terminal display of game

--- script.js
+++ script.js
@@ -14,45 +14,42 @@ currentPlayer = player1;
 
 // Create gameboard as an array inside of object
 const gameBoard = {
-    board: ['','','','','','','','',''],
+    board: [' ',' ',' ',' ',' ',' ',' ',' ','' ],
     display: function() {
-        console.log(`${this.board[0]}   | ${this.board[1]} |   ${this.board[2]}`)
-        console.log(`----------`)
-        console.log(`${this.board[3]}   | ${this.board[4]} |   ${this.board[5]}`)
-        console.log(`----------`)
-        console.log(`${this.board[6]}   | ${this.board[7]} |   ${this.board[8]}`)
+        console.log(`${this.board[0]} | ${this.board[1]} | ${this.board[2]}`)
+        console.log(`---------`)
+        console.log(`${this.board[3]} | ${this.board[4]} | ${this.board[5]}`)
+        console.log(`---------`)
+        console.log(`${this.board[6]} | ${this.board[7]} | ${this.board[8]}`)
     },
     reset: function() {
-        this.board = ['','','','','','','','',''];
+        this.board = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
     }
 };
-
-// Render game board in terminal
-gameBoard.display();
 
 // Function that receives player input and switches between players after each move
 const readline = require("readline");
 
-const rl = readline.createInterface({
+let rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
 
 function startGame() {
     if (!gameOver) {
-        rl.question(`${currentPlayer.name}, enter your move (0-8): `, (move) => {
+        rl.question(`${currentPlayer.name}, enter your move (1-9): `, (move) => {
             playerMove(parseInt(move));
         });
     }
 }
 
 function playerMove(position) {
-    if (gameBoard.board[position] === '') {
-        gameBoard.board[position] = currentPlayer.marker;
+    if (gameBoard.board[position - 1] === ' ') {
+        gameBoard.board[position - 1] = currentPlayer.marker;
         gameBoard.display();
         if (checkGameOver()) {
             gameOver = true;
-            readline.close()
+            rl.close()
         } else {
             switchPlayer();
             startGame();
@@ -80,7 +77,7 @@ function checkWins() {
         [0,4,8], [2,4,6]
     ]
 
-    for (patterns in wins) {
+    for (const patterns in wins) {
         const [a,b,c] = patterns;
         if (gameBoard.board[a] && gameBoard.board[a] === gameBoard.board[b] && gameBoard.board[a] === gameBoard.board[c]) {
             console.log(`${currentPlayer.name} wins!`)
@@ -91,7 +88,7 @@ function checkWins() {
 }
 
 function checkTies() {
-    if (!gameBoard.board.includes('')) {
+    if (!gameBoard.board.includes(' ')) {
         console.log("It's a tie!");
         return true;
     }
